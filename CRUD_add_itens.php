def2eb23029ab9b7290cca7fdb58aed11908d179
b3rn4rd0m3y3r <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-03-04 16:48:38 -0300

    Add files via upload

--- CRUD_add_itens.php
+++ CRUD_add_itens.php
@@ -8,7 +8,17 @@
 	$jsonObj = json_decode($json);
 	$json1 = $jsonObj->json1;
 	$json1Obj = json_decode($json1);
+	$json2 = $jsonObj->json2;
+	$json2Obj = json_decode($json2);
+	$json3 = $jsonObj->json3;
+	$json3Obj = json_decode($json3);
+	$json4 = $jsonObj->json4;
+	$json4Obj = json_decode($json4);
+	$json5 = $jsonObj->json5;
+	$json5Obj = json_decode($json5);
 	$NomePlan = $json1Obj[0]->NomePlan;
+	
+
 	echo $json;
 
 	$NoItens = count($jsonObj);
@@ -34,9 +44,13 @@
 			//$TpItem = mb_convert_encoding($jsonObj[$i]->TipodoItem, 'ISO-8859-1', 'auto');
 	//		}
 	// Arquivo de log e itens
-	$fp = fopen($NomePlan.'.txt', 'w');
+	$fp = fopen($NomePlan, 'w');
 	//fwrite($fp, $S ."\r\n");
 	fwrite($fp, mb_convert_encoding($json1, 'ISO-8859-1', 'auto')."\r\n");
+	fwrite($fp, mb_convert_encoding($json2, 'ISO-8859-1', 'auto')."\r\n");
+	fwrite($fp, mb_convert_encoding($json3, 'ISO-8859-1', 'auto')."\r\n");
+	fwrite($fp, mb_convert_encoding($json4, 'ISO-8859-1', 'auto')."\r\n");
+	fwrite($fp, mb_convert_encoding($json5, 'ISO-8859-1', 'auto')."\r\n");
 	//fwrite($fp, $itens);
 	//fwrite($fp, "No. Itens: " . $NoItens ."\r\n");
 	
